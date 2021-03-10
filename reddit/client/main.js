18fcbc73d2b3db1dede1866979c3f3e38162efa3
Oliver <smitho3@mailbo.winthro.edu>
2021-03-10 12:36:36 -0500

    mongo stuff

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -1,22 +1,38 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {UP_Collection_Access} from './../imports/api/user_posts.js';
 
-import './main.html';
+Tracker.autorun(function(){
+  console.log(UP_Collection_Access.find().fetch());
+})
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const renderPosts = function(passed) {
+  let formattedPost = passed.map(function(post){
+    return <p key={post._id}>{post.topic} have {post.votes} vote[s]</p>;
+  });
+  return formattedPost;
+};
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+Meteor.startup(function(){
+  UP_Collection_Access.insert({
+    topic: 'kids',
+    votes: 2000,
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  Tracker.autorun(function(){
+    const posts = UP_Collection_Access.find().fetch();
+    let title = '441 Reddit';
+    let jsx = (
+                <div>
+                  <h1>{title}</h1>
+                  {renderPosts(posts)}
+                </div>
+              );
+
+              ReactDOM.render(jsx, document.getElementById('content'));
+  });
+
+
+
 });
--- reddit/server/main.js
+++ reddit/server/main.js
@@ -1,5 +1,15 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+import {UP_Collection_Access} from './../imports/api/user_posts.js';
+
+Meteor.startup(function(){
+  UP_Collection_Access.insert({
+    topic: 'dog',
+    votes: 9,
+  });
+  UP_Collection_Access.insert({
+    topic: 'birds',
+    votes: 11,
+  });
+  console.log(UP_Collection_Access.find().fetch());
 });
